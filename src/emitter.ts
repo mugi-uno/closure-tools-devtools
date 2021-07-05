e2ff027fc761aebf3a4aeb23eb85d3443a2b0cb9
mugi-uno <mugi.uno@gmail.com>
2021-07-05 13:37:43 +0900

    Remove unused file

--- src/emitter.ts
+++
@@ -1,8 +0,0 @@
-import mitt, { Emitter } from "mitt";
-
-export const Events = {
-  EnableHighlight: {},
-  DisableHighlight: {},
-} as const;
-
-export const emitter: Emitter<typeof Events> = mitt<typeof Events>();
